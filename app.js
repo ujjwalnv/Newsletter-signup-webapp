ba245f24b60d3c2ece670bbe838ebe8b0bbb330d
Ujjwal Verma <vujjwal1n@gmail.com>
2022-07-29 12:25:48 +0530

    Create Newsletter Signup Webapp

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = process.env.MAILCHIMP_URL;
+  const options = {
+    method: "POST",
+    auth: process.env.AUTHKEY,
+  };
+  const request = https.request(url, options, function (response) {
+    var status = response.statusCode;
+    if (status === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      // console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000");
+});
+
